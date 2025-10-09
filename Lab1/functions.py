e964d9c61627379dc2cbf8b5931c9b5fead5d9a2
VickersSnickers <trojanhorsecard@gmail.com>
2025-10-09 10:07:37 +0300

    Initial commit

---
+++ Lab1/functions.py
@@ -0,0 +1,110 @@
+from random import *
+import csv
+from datetime import datetime, timedelta
+
+def load_names(source):
+    Male, Female = [[], [], []], [[], [], []]
+    with open(source, encoding="utf-8-sig") as f:
+        reader = csv.reader(f, delimiter=';')
+        next(reader)
+        for row in reader:
+            if row[3] == 'М':
+                Male[0].append(row[0])
+                Male[1].append(row[1])
+                Male[2].append(row[2])
+            else:
+                Female[0].append(row[0])
+                Female[1].append(row[1])
+                Female[2].append(row[2])
+    return Male, Female
+
+
+def generate_fullname(gender, Male, Female):
+    if gender == 0:
+        surname, name, patronym = choice(Male[0]), choice(Male[1]), choice(Male[2])
+    else: surname, name, patronym = choice(Female[0]), choice(Female[1]), choice(Female[2])
+    return f'{surname} {name} {patronym}'
+
+
+
+def generate_snils():
+    n = [randrange(10) for i in range(9)]
+    c = sum([n[-i]*i for i in range(9, 0, -1)])
+    while c > 101:
+        c -= 101
+    if (c == 100 or c == 101): c = 00
+    res = ''.join([str(i) for i in n])
+    return res[0:3] + '-' + res[3:6] + '-' + res[6:9] + ' ' + str(c).zfill(2)
+
+
+def generate_passport():
+    series_1 = ['01','03','04','05','07','08','10','11','14','15','17','18','19','20',
+        '22','24','25','26','27','28','29','30','32','33','34','36','37','38',
+        '40','41','42','44','45','46','47','49','50','52','53','54','56','57',
+        '58','60','61','63','64','65','66','68','69','70','71','73','75','76',
+        '77','79','80','81','82','83','84','85','86','87','88','89','90','91',
+        '92','93','94','95','96','97','98','99',]
+    series_2 = ['27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38',
+            '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
+            '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', 
+            '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', 
+            '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', 
+            '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', 
+            '99', '00', '01', '02', '03', '04', '05', '06', '07']
+    return f'="{series_1[randrange(len(series_1))] + series_2[randrange(len(series_2))] + 
+                ''.join([str(randrange(10)) for i in range(6)])}"'
+
+
+def generate_datetime(time_begin="08:00", time_end="21:00"):
+    time_begin_elem = datetime.strptime(time_begin, "%H:%M").time()     # time(8, 0)
+    time_end_elem = datetime.strptime(time_end, "%H:%M").time()         # time(21, 0)
+
+    start, end = datetime(2025, 1, 1), datetime(2025, 12, 31)
+    time_gap = end - start
+
+    while True:
+        rday = randint(0, time_gap.days)
+        rdate = start + timedelta(days=rday)
+        if rdate.weekday() < 5:                                         # 0-4 -> Пн-Пт
+            break
+    
+    min_seconds = time_begin_elem.minute * 60 + time_begin_elem.hour * 3600
+    max_seconds = time_end_elem.minute * 60 + time_end_elem.hour * 3600
+    rseconds = randint(min_seconds, max_seconds)
+    rtime = (datetime.min + timedelta(seconds=rseconds)).time()
+    
+    rdatetime = datetime.combine(rdate, rtime)
+    return rdatetime.strftime("%Y-%m-%dT%H:%M")
+
+
+def get_back_analysis(date_time, time_begin="08:00", time_end="21:00"):
+    time_begin_elem = datetime.strptime(time_begin, "%H:%M").hour
+    time_end_elem = datetime.strptime(time_end, "%H:%M").hour
+    dt = datetime.strptime(date_time, "%Y-%m-%dT%H:%M")
+    while True:
+        hours_to_add = uniform(24, 72)
+        new_dt = dt + timedelta(hours=hours_to_add)
+        if new_dt.weekday() < 5:
+            if new_dt.hour < time_begin_elem:
+                new_dt = new_dt.replace(hour=time_begin_elem, minute=0, second=0, microsecond=0)
+            elif new_dt.hour >= time_end_elem:
+                new_dt = new_dt.replace(hour=time_end_elem-1, minute=59, second=0, microsecond=0)
+            return new_dt.strftime("%Y-%m-%dT%H:%M")
+        
+
+def calculate_luhn_checksum(partial_card_number):
+    digits = [int(d) for d in partial_card_number]
+    for i in range(len(digits)-1, -1, -2):
+        digits[i] *= 2
+        if digits[i] > 9:
+            digits[i] -= 9
+    total = sum(digits)
+    checksum = (10 - (total % 10)) % 10
+    return str(checksum)
+
+def generate_card_number(banks):
+    chosen_bank = choices(range(len(banks)), weights=[bank[2] for bank in banks], k=1)[0]
+    system_name, bin_list = choice(banks[chosen_bank][1])
+    bin_code = choice(bin_list)
+    partial_num = bin_code + ''.join(str(randrange(9)) for i in range(9))
+    return '="' + partial_num + calculate_luhn_checksum(partial_num) + '"'
---
+++ Lab1/main.py
@@ -0,0 +1,150 @@
+from functions import *
+import argparse
+
+def make_medcard(bank_card_data, male_list, female_list):
+
+    total = []
+    
+    """Случайное количество случайных записей из medicine.csv в виде списка seed"""
+    rnum = randint(3, 10)
+    seed = [medicine[randrange(med_num)] for i in range(rnum)] 
+
+    name = generate_fullname(randrange(2), male_list, female_list)
+    passport = generate_passport()
+    snils = generate_snils()
+    card = generate_card_number(bank_card_data)
+
+    counter, flag = 0, True
+    for elem in seed:
+
+        """Генерируем новую карту если использована больше 5 раз"""
+        counter += 1
+        if counter > 5 and flag:
+            card = generate_card_number(bank_card_data)
+            flag = False
+
+        data = dict()
+        analysis = []
+        """ Случайное количество анализов (от 3 до 5), соответствующее симптому"""
+        temp = elem[1].split('|')
+        al = [temp[randrange(len(temp))] for i in range(3, 5)]
+        for _ in al:
+            analysis.append(_)
+
+        temp = elem[2].split(',')
+
+        al_cost = 0
+        for i in analysis:
+            """Для каждого анализа ищем стоймость в costs.csv и суммируем в an_cost"""
+            for line in costs:
+                if line[0] == i:
+                    al_cost += int(line[1])
+                    break
+
+        date = generate_datetime()
+
+        data["ФИО"] = name
+        data["Паспортные данные"] = passport
+        data["СНИЛС"] = snils
+        data["Симптомы"] = elem[0]
+        """Случайный доктор, соответствующий симптому"""
+        data["Выбор врача"] = temp[randrange(len(temp))]
+        data["Дата посещения врача"] = date
+        data["Анализы"] = ' | '.join(analysis)
+        data["Дата получения анализов"] = get_back_analysis(date)
+        data["Стоимость анализов"] = al_cost
+        data["Карта оплаты"] = card
+
+        total.append(data)
+    return total
+
+def get_csv(source):
+    lst = []
+    with open(source, newline="", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f, delimiter=";")
+        for row in reader:
+            temp = [row[elem] for elem in reader.fieldnames]
+            lst.append(temp)
+    return lst
+
+def output(source, data):
+    with open(source, "w", newline="", encoding="utf-8-sig") as f:
+        head = ["ФИО", "Паспортные данные", "СНИЛС", "Симптомы", "Выбор врача",
+                "Дата посещения врача", "Анализы", "Дата получения анализов",
+                "Стоимость анализов", "Карта оплаты"]
+        writer = csv.DictWriter(f, fieldnames=head, delimiter=';')
+        writer.writeheader()
+        writer.writerows(data)
+
+def generate_output_csv(source, bank_card_data, male_list, female_list, limit):
+    out = []
+    counter = limit
+    while counter > 0:
+        newline = make_medcard(bank_card_data=bank_card_data, male_list=male_list,
+                             female_list=female_list)
+        out += newline
+        counter -= len(newline)
+    out = out[:limit]
+    output(source=source, data=out)
+
+
+def set_input():
+    parser = argparse.ArgumentParser(description="Генерация медицинских карт")
+    parser.add_argument("-n", "--num", type=int, default=50000,
+                        help="Количество строк в выходном файле (по умолчанию 50000)")
+    parser.add_argument("-w", "--weights", nargs=5, type=float,
+                        default=[0.3, 0.2, 0.2, 0.2, 0.1],
+                        help="Веса банков в формате: w1 w2 w3 w4 w5 (сумма = 1)")
+    
+    args = parser.parse_args()
+    weights = args.weights
+
+    card_data = [
+        ["Сбербанк", [
+            ["Мир", ["220100", "220220"]],
+            ["Visa", ["427402", "427406", "427411"]],
+            ["Mastercard", ["559901", "559521", "557000"]],
+            ["UnionPay", ["623371", "623372"]]
+        ], weights[0]],
+
+        ["Тинькофф", [
+            ["Мир", ["220070"]],
+            ["Visa", ["415428", "415429"]],
+            ["Mastercard", ["553691", "553420", "551960"]],
+            ["UnionPay", ["623373"]]
+        ], weights[1]],
+
+        ["ВТБ", [
+            ["Мир", ["220024"]],
+            ["Visa", ["489195", "489196"]],
+            ["Mastercard", ["518704", "518373"]],
+            ["UnionPay", ["623374"]]
+        ], weights[2]],
+
+        ["АльфаБанк", [
+            ["Мир", ["220015"]],
+            ["Visa", ["410584", "415400"]],
+            ["Mastercard", ["555949"]],
+            ["UnionPay", ["623375"]]
+        ], weights[3]],
+
+        ["ГазпромБанк", [
+            ["Мир", ["220001"]],
+            ["Visa", ["404136", "404270"]],
+            ["Mastercard", ["539839", "544026"]],
+            ["UnionPay", ["623376"]]
+        ], weights[4]]
+    ]
+
+    return args.num, card_data
+
+if  __name__ == "__main__":
+
+    num, card_data = set_input()
+    MALE, FEMALE = load_names('data/names.csv')
+    medicine = get_csv('data/medicine.csv')
+    med_num = len(medicine)
+    costs = get_csv('data/costs.csv')
+    cost_num = len(costs)
+
+    generate_output_csv('output.csv', card_data, MALE, FEMALE, num)
