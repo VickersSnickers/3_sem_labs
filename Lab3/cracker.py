3085305d3426946c7362e8d8066d680847b6f147
VickersSnickers <trojanhorsecard@gmail.com>
2025-11-01 00:25:12 +0300

    first code version added

--- Lab3/cracker.py
+++ Lab3/cracker.py
@@ -3,36 +3,44 @@ import subprocess
 from pathlib import Path
 import re
 
-INPUT = "input.xlsx"
-OUTPUT = "cracked.xlsx"
-HASHES_TXT = "hashes.txt"
-HASHCAT_EXE = r"E:\ProgramFiles\hashcat-7.1.2\hashcat.exe"
+# пути
+BASE_DIR = Path(__file__).parent
+HASHCAT_EXE = Path(r"E:\ProgramFiles\hashcat-7.1.2\hashcat.exe")
+
+INPUT = BASE_DIR / "data/data_вар. 1.xlsx"
+HASHES_TXT = BASE_DIR / "hashes.txt"
+OUTPUT_TXT = BASE_DIR / "cracked.txt"
+OUTPUT_XLSX = BASE_DIR / "cracked.xlsx"
+
 MASK = "?d?d?d?d?d?d?d?d?d?d?d"
 
-# читаем хэши из Excel
+# читаем хэши
 df = pd.read_excel(INPUT, dtype=str).fillna("")
-hashes = [h.strip().lower() for h in df.iloc[:,0] if re.fullmatch(r"[0-9a-f]{32}", h.strip().lower())]
+hashes = [h.strip().lower() for h in df.iloc[:, 0] if re.fullmatch(r"[0-9a-f]{32}", h.strip().lower())]
 
 # сохраняем в txt
-# with open(HASHES_TXT, "w") as f:
-#     f.write("\n".join(hashes))
+with open(HASHES_TXT, "w") as f:
+    f.write("\n".join(hashes))
 
-# получаем ID NVIDIA-устройств
-device_ids = ["2"]
-
-# запускаем hashcat
+# команды hashcat
 cmd = [
-    HASHCAT_EXE,
-    "-m", "0",              # MD5
-    "-a", "3",              # маска
-    "-o", str(OUTPUT), # выходной файл
-    HASHES_TXT,
-    MASK,
-    "--opencl-device-types", "2"  # GPU только
+    str(HASHCAT_EXE),
+    "-m", "0",
+    "-a", "3",
+    "-o", str(OUTPUT_TXT),
+    "--show",
+    "-O",
+    str(HASHES_TXT),
+    MASK
 ]
+
+# запускаем hashcat (рабочая директория — та же, где Python-скрипт)
 subprocess.run(cmd, cwd=Path(HASHCAT_EXE).parent)
 
-# читаем результат и сохраняем в Excel
-cracked = pd.read_csv("cracked.txt", sep=":", header=None, names=["hash","password"])
-cracked.to_excel(OUTPUT, index=False)
-print(f"Saved cracked hashes to {OUTPUT}")
+# читаем результаты
+if OUTPUT_TXT.exists():
+    cracked = pd.read_csv(OUTPUT_TXT, sep=":", header=None, names=["hash", "password"])
+    cracked.to_excel(OUTPUT_XLSX, index=False)
+    print(f"✅ Saved cracked hashes to {OUTPUT_XLSX}")
+else:
+    print("⚠️ Hashcat did not produce an output file.")
